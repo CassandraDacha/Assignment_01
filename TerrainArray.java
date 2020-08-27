5c82adb578feb6d1bc0c954cb6098415b24b799d
Cassandra Dacha <44982109+CassandraDacha@users.noreply.github.com>
2020-08-27 18:18:01 +0200

    Add files via upload

--- TerrainArray.java
+++ TerrainArray.java
@@ -1,44 +1,48 @@
+import java.util.ArrayList;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
-class TerrainArray extends RecursiveTask<Integer> {
-    static int SEQUENTIAL_THRESHOLD = 1000;
-    
-    int lo;
-    int hi;
-    int[] arr;
-    TerrainArray(int[] a, int l, int h) { lo=l; hi=h; arr=a; }
-    
-    public ArrayList<String> compute() {
-    if(hi - lo <= SEQUENTIAL_THRESHOLD) {
-        int ans = 0;
-        int[][] indexes = new int[1000][1000];
-        ArrayList<String> list_of_indexes = new ArrayList<>();
-        for(int i=lo; i < hi; ++i)
-            for(int j=lo; j < hi; ++j)
-                if ( i! = 0|| j! = 0 || j! = numrows||i!= numrows)
-                    if (arr[i][j] < arr[i-1][j-1]&&arr[i-1][j]&&arr[i-1][j+1]&&arr[i][j-1]&&arr[i][j+1]&&arr[i+1][j-1]&&arr[i+1][j]&&arr[i+1][j+1])
-                    {
-                       String indexes = i+","+j;
-                       list.add(indexes);
-                 
-                       
-                    }
-                   return list_of_indexes;
-        } else {
-            TerrainArray left = new TerrainArray(arr,lo,(hi+lo)/2);
-            TerrainArray right = new TerrainArray(arr,(hi+lo)/2,hi);
-            left.fork();
-            ArrayList<String> rightAns = right.compute();
-            ArrayList<String> leftAns = left.join();
-	    rightAns.addAll(leftAns); 
-            return rightAns;
-        }
-     }
- }
-class Main {
-    static int TerrainArray(int[] array)
-{
-        return ForkJoinPool.commonPool().invoke(new TerrainArray(array,0,array.length));
-        
-}
+
+class TerrainArray extends  RecursiveTask<ArrayList<String>> {
+    static final int SEQUENTIAL_THRESHOLD = 1200;
+
+    public int lo;
+    public int hi;
+    public double[][] basinarr;
+
+    TerrainArray(double[][] a, int l, int h) {
+         lo=l; hi=h;
+        basinarr = a;
     }
+
+    protected ArrayList<String> compute() {
+        if(hi - lo <= SEQUENTIAL_THRESHOLD) {
+            int ans = 0;
+            ArrayList<String> list_of_indexes = new ArrayList<>();
+            for(int i=lo; i < hi; ++i){
+                for(int j=lo; j < hi; ++j){
+                    if (i != 0 &&j != 0 && j != basinarr.length-1 &&i != basinarr.length-1){
+                        if ((basinarr[i][j] +0.01)<= basinarr[i - 1][j - 1] && (basinarr[i][j] +0.01) <= basinarr[i - 1][j ] && (basinarr[i][j] +0.01) <= basinarr[i-1][j + 1] && (basinarr[i][j] +0.01) <= basinarr[i][j - 1] && (basinarr[i][j] +0.01) <= basinarr[i][j+1] && (basinarr[i][j] +0.01) <= basinarr[i + 1][j - 1] && (basinarr[i][j] +0.01) <= basinarr[i+1][j] && (basinarr[i][j] +0.01) <= basinarr[i + 1][j + 1]) {
+                            String indexes = i + " " + j;
+                                list_of_indexes.add(indexes);
+                    }
+                    }
+                }
+            }
+            return list_of_indexes;
+        }
+        else {
+            int mid = lo + (hi - lo) / 2;
+            TerrainArray left = new TerrainArray(basinarr,lo,mid);
+
+            TerrainArray right = new TerrainArray(basinarr,mid,hi);
+            left.fork();
+
+            ArrayList <String> rightAns = right.compute();
+            ArrayList <String> leftAns = left.join();
+
+
+            leftAns.addAll(rightAns);
+            return leftAns;
+        }
+    }
+}
